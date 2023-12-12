cb0e42b9528fe6d6f2857c3074c85f537ca78698
VButenk0 <vladflash10@gmail.com>
2023-12-12 12:36:40 +0200

    hw4-1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,82 @@
-import { React, Component } from 'react';
+import { React, useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickGood = () => {
+    setGood(prevState => prevState + 1);
   };
 
-  onLeaveFeedback = e => {
-    const { name } = e.target;
-
-    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  const onClickNeutral = () => {
+    setNeutral(prevState => prevState + 1);
   };
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+  const onClickBad = () => {
+    setBad(prevState => prevState + 1);
+  };
+
+  const countTotalFeedback = () => {
     return good + neutral + bad;
-  }
+  };
 
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    return total ? ((this.state.good / total) * 100).toFixed() : 0;
-  }
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? ((good / total) * 100).toFixed() : 0;
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.onLeaveFeedback}
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          // options={['good', 'neutral', 'bad']}
+          onClickGood={onClickGood}
+          onClickNeutral={onClickNeutral}
+          onClickBad={onClickBad}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
+  // onLeaveFeedback = e => {
+  //   const { name } = e.target;
+
+  //   this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  // };
+
+  // countTotalFeedback() {
+  //   const { good, neutral, bad } = this.state;
+  //   return good + neutral + bad;
+  // }
+
+  // countPositiveFeedbackPercentage() {
+  //   const total = this.countTotalFeedback();
+  //   return total ? ((this.state.good / total) * 100).toFixed() : 0;
+  // }
+
+  // render() {
+  //   const { good, neutral, bad } = this.state;
+  //   const total = this.countTotalFeedback();
+  //   const positivePercentage = this.countPositiveFeedbackPercentage();
+
+  // }
+};
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,28 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { OptionsContainer, OptionsButton } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+export const FeedbackOptions = ({
+  // options,
+  onClickGood,
+  onClickNeutral,
+  onClickBad,
+}) => {
   return (
     <OptionsContainer>
-      {options.map(option => (
+      {/* {options.map(option => (
         <OptionsButton key={nanoid()} name={option} onClick={onLeaveFeedback}>
           {option}
         </OptionsButton>
-      ))}
+      ))} */}
+      <OptionsButton name="good" onClick={onClickGood}>
+        Good
+      </OptionsButton>
+      <OptionsButton name="neutral" onClick={onClickNeutral}>
+        Neutral
+      </OptionsButton>
+      <OptionsButton name="bad" onClick={onClickBad}>
+        Bad
+      </OptionsButton>
     </OptionsContainer>
   );
 };
